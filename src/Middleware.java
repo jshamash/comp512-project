21b672302eedb35be35a4de824bb1dfd07a4f7b1
Jake Shamash <jshamash@inocybe.ca>
2013-09-21 20:46:24 -0400

    Middleware framework

--- src/Middleware.java
+++ src/Middleware.java
@@ -1,24 +1,30 @@
 import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Vector;
 
+import ResImpl.ResourceManagerImpl;
 import ResInterface.ResourceManager;
 
-public class Middleware {
+public class Middleware implements ResourceManager {
 
 	static ResourceManager flightRM = null;
 	static ResourceManager carRM = null;
 	static ResourceManager roomRM = null;
+	
+	public Middleware() throws RemoteException {}
 
 	public static void main(String args[]) {
 		String flightServer, carServer, roomServer;
-		int flightPort, carPort, roomPort;
+		int flightPort, carPort, roomPort, rmiPort;
 
-		if (args.length != 6) {
+		if (args.length != 7) {
 			System.err.println("Wrong usage");
 			System.out
 					.println("Usage: java ResImpl.ResourceManagerImpl [flightServer] [flightPort] "
-							+ "[carServer] [carPort] [roomServer] [roomPort]");
+							+ "[carServer] [carPort] [roomServer] [roomPort] [RMI port]");
 			System.exit(1);
 		}
 
@@ -28,6 +34,7 @@ public class Middleware {
 		carPort = Integer.parseInt(args[3]);
 		roomServer = args[4];
 		roomPort = Integer.parseInt(args[5]);
+		rmiPort = Integer.parseInt(args[6]);
 
 		try {
 			// get a reference to the flight rmiregistry
@@ -63,6 +70,19 @@ public class Middleware {
 			} else {
 				System.out.println("Unsuccessful");
 			}
+			
+			// At this point we are a client to the three RM servers.
+			// Now, establish server connection to serve client(s).
+			Middleware obj = new Middleware();
+			// dynamically generate the stub (client proxy)
+			ResourceManager rm = (ResourceManager) UnicastRemoteObject
+					.exportObject(obj, 0);
+
+			// Bind the remote object's stub in the registry
+			registry = LocateRegistry.getRegistry(rmiPort);
+			registry.rebind("Group1ResourceManager", rm);
+
+			System.err.println("Server ready");
 		} catch (Exception e) {
 			System.err.println("Middleware exception: " + e.toString());
 			e.printStackTrace();
@@ -72,4 +92,114 @@ public class Middleware {
 			System.setSecurityManager(new RMISecurityManager());
 		}
 	}
+
+	public boolean addFlight(int id, int flightNum, int flightSeats,
+			int flightPrice) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean addCars(int id, String location, int numCars, int price)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean addRooms(int id, String location, int numRooms, int price)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int newCustomer(int id) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean newCustomer(int id, int cid) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteFlight(int id, int flightNum) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteCars(int id, String location) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteRooms(int id, String location) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteCustomer(int id, int customer) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int queryFlight(int id, int flightNumber) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int queryCars(int id, String location) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int queryRooms(int id, String location) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String queryCustomerInfo(int id, int customer)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int queryFlightPrice(int id, int flightNumber)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int queryCarsPrice(int id, String location) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int queryRoomsPrice(int id, String location) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean reserveFlight(int id, int customer, int flightNumber)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean reserveCar(int id, int customer, String location)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean reserveRoom(int id, int customer, String locationd)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean reserveItinerary(int id, int customer, Vector flightNumbers,
+			String location, boolean Car, boolean Room) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
