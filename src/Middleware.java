06cf51f631bb06cde7f5ef6780fff6e1bb3ed1b9
jizzblitzkrieg <jeremiebedard@hotmail.com>
2013-09-21 20:03:19 -0400

    Main method Middleware

--- src/Middleware.java
+++ src/Middleware.java
@@ -1,8 +1,49 @@
+import java.rmi.RMISecurityManager;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.lang.String;
+
+import ResImpl.ResourceManagerImpl;
+import ResInterface.ResourceManager;
+
 
 public class Middleware {
-	// Jeremie says hi
-	public void UsefulFunction(boolean isTrue)
-	{
 	
-	}
+	//Jake, look! THis is the main method!
+	public static void main(String args[]) {
+        // Figure out where server is running
+        String server = "teaching";
+        int port = 6969;
+
+        if (args.length == 1) {
+            server = server + ":" + args[0];
+            port = Integer.parseInt(args[0]);
+        } else if (args.length != 0 &&  args.length != 1) {
+            System.err.println ("Wrong usage");
+            System.out.println("Usage: java ResImpl.ResourceManagerImpl [port]");
+            System.exit(1);
+        }
+
+        try {
+            // create a new Server object
+            ResourceManagerImpl obj = new ResourceManagerImpl();
+            // dynamically generate the stub (client proxy)
+            ResourceManager rm = (ResourceManager) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry(port);
+            registry.rebind("MyGroupResourceManager", rm);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+
+        // Create and install a security manager
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new RMISecurityManager());
+        }
+    }
 }
